0c887bbac4d412365414f697ece449f182906811
me@eugenel.in <me@eugenel.in>
2019-11-16 13:23:38 +0000

    Added centroid setup

--- ktmeans/main.kt
+++ ktmeans/main.kt
@@ -1,8 +1,21 @@
 import java.io.File
 import java.awt.image.*
 import javax.imageio.*
+import kotlin.random.*
 
 val k = 5
+val clusters = Array(5) {
+    mutableListOf<Pair<Int, Int>>()
+}
+val centroids = Array(k) {
+    Centroid(
+        it,
+        Random.nextInt(0, 256),
+        Random.nextInt(0, 256),
+        Random.nextInt(0, 256)
+    )
+}
+
 
 // consider each point as a r3 vector
 fun main(args: Array<String>) {
@@ -11,6 +24,9 @@ fun main(args: Array<String>) {
     val img = ImageIO.read(File(input))
 }
 
-fun iterate() {
-
-}
+data class Centroid(
+    var id: Int,
+    var r: Int,
+    var g: Int,
+    var b: Int
+)
