a9129358cb7228517cd27a2961aceb99ed5f8317
me@eugenel.in <me@eugenel.in>
2019-11-16 12:52:43 +0000

    Started basic image loading in Kotlin

---
+++ ktmeans/main.kt
@@ -0,0 +1,16 @@
+import java.io.File
+import java.awt.image.*
+import javax.imageio.*
+
+val k = 5
+
+// consider each point as a r3 vector
+fun main(args: Array<String>) {
+    val input = "img.jpeg"
+    val output = "out.jpeg"
+    val img = ImageIO.read(File(input))
+}
+
+fun iterate() {
+
+}
